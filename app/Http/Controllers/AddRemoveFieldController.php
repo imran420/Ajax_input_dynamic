e30cba7d95797f2499addd11bedc56c22ca77e87
Imran Hossain <md.imranhi88@gmail.com>
2021-01-13 21:07:43 +0600

    init

---
+++ app/Http/Controllers/AddRemoveFieldController.php
@@ -0,0 +1,38 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Todo;
+
+use Illuminate\Support\Facades\Validator;
+
+class AddRemoveFieldController extends Controller
+{
+    public function index()
+    {
+        return view("add-remove-input-fields");
+    }
+    public function store(Request $request) {
+
+        $data = [];
+
+        foreach($request->input('title') as $key => $value) {
+            $data["title.{$key}"] = 'required';
+        }
+
+        $validator = Validator::make($request->all(), $data);
+
+        if ($validator->passes()) {
+
+            foreach($request->input('title') as $key => $value) {
+                Todo::create(['title'=>$value]);
+            }
+
+            return response()->json(['success'=>'true']);
+        }
+
+        return response()->json(['error'=>$validator->errors()->all()]);
+    }
+
+}
